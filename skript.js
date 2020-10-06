ddc5006c1a87adc657b1cafbd4c5a0055fd1f15d
Mac <mac@MacdeMacBook-Pro.local>
2020-10-06 19:23:49 +0200

    Motion

--- skript.js
+++ skript.js
@@ -1,3 +1,28 @@
+ var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+     lastTop;
+
+ window.addEventListener('scroll', function (event) {
+     var train = document.getElementById('trainMotion'),
+         top = train.getBoundingClientRect().top,
+         offset = top - windowHeight;
+
+     if (offset > 0) {
+         train.classList.remove('choochoo');
+         return;
+     }
+
+     if (top < windowHeight / 2 && top > lastTop) {
+         train.classList.remove('choochoo');
+     }
+
+     if (train.className.indexOf('choocho') === -1 && top < lastTop) {
+         train.classList.add('choochoo');
+     }
+
+     lastTop = top;
+
+ });
+
  $(window).scroll(function () {
 
      // selectors
